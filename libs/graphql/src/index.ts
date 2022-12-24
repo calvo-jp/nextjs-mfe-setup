a948dcab5be9ff6b16e5672732ad29d1024058c4
calvo-jp <calvojp92@gmail.com>
2022-12-25 00:00:18 +0800

    refactor

--- libs/graphql/src/index.ts
+++ libs/graphql/src/index.ts
@@ -1,16 +1,18 @@
 export { default as constants } from "./lib/constants";
 export {
   CountriesDocument,
-  CountriesQuery,
-  CountriesQueryVariables,
-  Country,
   CountryDocument,
-  CountryQuery,
-  CountryQueryVariables,
   useCountriesLazyQuery,
   useCountriesQuery,
   useCountryLazyQuery,
   useCountryQuery,
+} from "./lib/generated";
+export type {
+  CountriesQuery,
+  CountriesQueryVariables,
+  Country,
+  CountryQuery,
+  CountryQueryVariables,
 } from "./lib/generated";
 export { default as graphqlClient } from "./lib/graphqlClient";
 export { default as GraphQLProvider } from "./lib/GraphQLProvider";
