4b3b72d74faaa189d89b0bf35302aef8b5c91257
calvo-jp <calvojp92@gmail.com>
2022-12-25 15:08:26 +0800

    simplify

--- libs/common/src/lib/nextAuthOptions.ts
+++ libs/common/src/lib/nextAuthOptions.ts
@@ -1,11 +1,11 @@
 import { NextAuthOptions } from "next-auth";
 
 const nextAuthOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-    maxAge: 60 * 59 * 24 * 30, // 30d - 1m,
-  },
   providers: [],
+  pages: {
+    error: "/login",
+    signIn: "/login",
+  },
 };
 
 export default nextAuthOptions;
