00beff896eb517f3911edd126e062571c4461b3f
calvo-jp <calvojp92@gmail.com>
2022-12-25 22:43:11 +0800

    init login

--- apps/host/pages/login.tsx
+++ apps/host/pages/login.tsx
@@ -1,3 +1,45 @@
+import { Box, Button, chakra, Input } from "@chakra-ui/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  email: z.string().email(),
+});
+
 export default function Login() {
-  return null;
+  const { register, handleSubmit, formState } = useForm<z.infer<typeof schema>>(
+    {
+      shouldFocusError: true,
+      shouldUnregister: true,
+      resolver: zodResolver(schema),
+      defaultValues: {
+        email: "",
+      },
+    },
+  );
+
+  return (
+    <Box>
+      <Box>
+        <chakra.form
+          noValidate
+          onSubmit={handleSubmit(async (data) => {
+            console.log(data);
+
+            // TODO
+            // login
+          })}
+        >
+          <Input type="email" {...register("email")} />
+          <Button
+            disabled={formState.isSubmitting}
+            isLoading={formState.isSubmitting}
+          >
+            Get OTP Code
+          </Button>
+        </chakra.form>
+      </Box>
+    </Box>
+  );
 }
